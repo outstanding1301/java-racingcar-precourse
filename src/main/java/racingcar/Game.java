1702bd74782016417f8cf5f70d10b2cdc208cc85
outstandingboy <outstanding1301@gmail.com>
2021-01-14 17:44:16 +0900

    feat: add Game(CarRepository repository, int trial) and methods (trying, printStatus)

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,4 +1,27 @@
 package racingcar;
 
 public class Game {
+    private CarRepository repository;
+    private int trial;
+
+    public Game(CarRepository repository, int trial) {
+        this.repository = repository;
+        this.trial = trial;
+    }
+
+    public int getTrial() {
+        return trial;
+    }
+
+    public void trying() {
+        for (Car car : repository.getCarList()) {
+            car.move();
+        }
+    }
+
+    public void printStatus() {
+        for (Car car : repository.getCarList()) {
+            System.out.printf("%s : %s\n", car.getName(), car.getPositionString());
+        }
+    }
 }
