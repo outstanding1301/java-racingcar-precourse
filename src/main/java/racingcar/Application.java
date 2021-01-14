f21a8f34aad86cdd9fc6fcc8dc982c190c476cf6
outstandingboy <outstanding1301@gmail.com>
2021-01-14 18:23:15 +0900

    feat: move createGame method from Application to Game

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,25 +7,8 @@ import java.util.stream.Collectors;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        Game game = createGame(scanner);
+        Game game = Game.createGame(new InputManager(scanner));
+        if (game == null) return;
         game.start();
-    }
-
-    public static Game createGame(Scanner scanner) throws NumberFormatException {
-        CarRepository repository = new CarRepositoryImpl();
-
-        System.out.print("경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분) > ");
-        String[] carNames = scanner.nextLine().split(",");
-        System.out.println();
-
-        System.out.print("시도할 회수 > ");
-        int trial = Integer.parseInt(scanner.nextLine());
-        System.out.println();
-
-        for (String carName : carNames) {
-            repository.addCar(new Car(carName));
-        }
-
-        return new Game(repository, trial);
     }
 }
