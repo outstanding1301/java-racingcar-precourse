5c86e275a9203eb472ad851b3fc19bc40e7ab739
outstandingboy <outstanding1301@gmail.com>
2021-01-14 17:47:29 +0900

    feat: add start method to start game

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Game {
     private CarRepository repository;
     private int trial;
@@ -23,5 +26,20 @@ public class Game {
         for (Car car : repository.getCarList()) {
             System.out.printf("%s : %s\n", car.getName(), car.getPositionString());
         }
+    }
+
+    public List<Car> getWinner() {
+        return repository.getWinner();
+    }
+
+    public void start() {
+        System.out.println("실행 결과");
+        for (int i=0; i<trial; i++) {
+            trying();
+            printStatus();
+            System.out.println();
+        }
+        String winner = String.join(", ", getWinner().stream().map(c -> c.getName()).collect(Collectors.toList()));
+        System.out.println("최종 우승자 : " + winner);
     }
 }
