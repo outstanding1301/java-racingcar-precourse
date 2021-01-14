f5fc979be7b0ebbf75854cfb9d217b3513793b06
outstandingboy <outstanding1301@gmail.com>
2021-01-14 17:48:54 +0900

    feat: add createGame function to create Game instance, and write main function

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,31 @@
 package racingcar;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        Game game = createGame(scanner);
+        game.start();
+    }
+
+    public static Game createGame(Scanner scanner) throws NumberFormatException {
+        CarRepository repository = new CarRepositoryImpl();
+
+        System.out.print("경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분) > ");
+        String[] carNames = scanner.nextLine().split(",");
+        System.out.println();
+
+        System.out.print("시도할 회수 > ");
+        int trial = Integer.parseInt(scanner.nextLine());
+        System.out.println();
+
+        for (String carName : carNames) {
+            repository.addCar(new Car(carName));
+        }
+
+        return new Game(repository, trial);
     }
 }
