60fefbb9cc4ee0691e160a90fa13c7468b50bab0
outstandingboy <outstanding1301@gmail.com>
2021-01-14 17:42:02 +0900

    feat: add move method and getters

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -8,5 +10,19 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public void move() {
+        int randInt = RandomUtils.nextInt(0, 9);
+        // move forward
+        if (randInt >= 4) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
